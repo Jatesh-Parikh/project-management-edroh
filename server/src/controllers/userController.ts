793bde2a5b227c02d80585363a5aa020c5e52e30
Jatesh Parikh <jateshparikh97@gmail.com>
2025-12-31 22:13:00 +0530

    add getUser controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -13,3 +13,20 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
       .json({ message: `Error retrieving users: ${error.message}` });
   }
 };
+
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+  const { cognitoId } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        cognitoId,
+      },
+    });
+
+    res.json(user);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving user: ${error.message}` });
+  }
+};
