c19c9fe2541dbaab53b412f64768e1ca51f4551d
Jatesh Parikh <jateshparikh97@gmail.com>
2026-02-24 15:20:43 +0530

    add ModalNewProject component

---
+++ client/src/app/projects/ProjectHeader.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import {
+  Clock,
+  Filter,
+  Grid3X3,
+  List,
+  PlusSquare,
+  Share2,
+  Table,
+} from "lucide-react";
+import { useState } from "react";
+import ModalNewProject from "./ModalNewProject";
